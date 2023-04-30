df8b512de6f3aa8100ca6f1574fb058e64ba2a0a
dfgravatas <gravatas.df.form.app@gmail.com>
2023-04-30 00:37:01 -0300

    Switch from createBrowserRouter to createHashRouter, in order to deploy to ghpages, since ghpages don't have support to BrowserRouter tech

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import './index.css'
 
 import { ClientForm } from "./pages/ClientForm";
 import { Form } from './pages/Form';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/ticket-forms",
     element: <App />,
