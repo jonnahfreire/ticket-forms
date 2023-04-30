0c157cfeb0c105950cb833fe944bc3724b7bb223
dfgravatas <gravatas.df.form.app@gmail.com>
2023-04-29 23:31:37 -0300

    Update main routes

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,7 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       {
-        path: "/ticket-forms/form",
+        path: "/ticket-forms/",
         element: <Form />,
       },
       {
